b918db9ffa61e3c7ced70b7f9b8683e69daf39d9
Vishakha3106 <gautamtashu201@gmail.com>
2026-04-29 18:40:25 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const obj={
+    firstName:"Vishakha",
+    age:"21",
+    location:"Himachal",
+    skills: {
+        languages:["js","java","kotlin"],
+        frameworksAndLibraries:["react","Android","redux"]
+    }
+};
+//shallow copy cons are nested object 
+// const copyObj =obj
+// console.log(obj)
+// copyObj.skills.frameworksAndLibraries.push("spring")
+// console.log(copyObj)
+
+//deep copy will take care of nested objects/arrays also
+//custom deep copy fucntion
+//put name_ in front of key of object
+function deepCopy(obj){
+    if(obj === null || typeof obj !=='object'){
+        return obj
+    }
+    if(Array.isArray(obj)){
+       return  obj.map((item)=>deepCopy(item))
+    }
+
+    let clone={};
+    for(let key in obj){
+        if(obj.hasOwnProperty(key)){
+            clone[`name_${key}`]=deepCopy(obj[key])
+        }
+    }
+    return clone
+}
+
+const copyObj=deepCopy(obj)
+console.log(obj)
+console.log(copyObj)
+copyObj.skills.frameworksAndLibraries.push("spring")
+console.log(copyObj)
+
+
+
